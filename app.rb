1f21b757e0ca3dd85ed3f07d63742f97c59df988
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-21 18:29:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post '/reverse' do
     post '/reverse' do
     original_string = params["string"]
-    reversed_string = original_string.reverse
+    @reversed_string = original_string.reverse
 
   erb :reversed
 end
