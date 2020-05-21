1a3ed1a1fca1cecdd410d5630f046f9659acc6ab
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-21 18:38:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,11 @@ class App < Sinatra::Base
 
   get '/friends' do
     # Write your code here!
+    get '/friends' do
+      @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+
+      erb :friends
+    end
 
   end
 end
